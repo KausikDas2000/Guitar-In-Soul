0b7b051ff68f6eac981a7484415ef0f40f194e35
kausikDas <kd96668@gmail.com>
2026-08-02 17:31:58 +0530

    / FIX

--- Server/app.js
+++ Server/app.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin:  [
       "http://localhost:5173",
-      "https://guitar-in-soul.vercel.app/",
+      "https://guitar-in-soul.vercel.app",
     ],
 
     credentials: true,
