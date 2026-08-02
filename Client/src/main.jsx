769250ad5f546c7daa0a404dc86185a97aed14c4
kausikDas <kd96668@gmail.com>
2026-08-02 20:35:08 +0530

    add analytics

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { HelmetProvider } from 'react-helmet-async'
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-EJRWV7WEGF");
 
 createRoot(document.getElementById('root')).render(
    <HelmetProvider>
