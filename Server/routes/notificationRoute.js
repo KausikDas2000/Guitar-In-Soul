0c42582b507c78338c43e84d1a4aa0f7800ceb25
kausikDas <kd96668@gmail.com>
2026-08-06 21:16:17 +0530

    protect fix

--- Server/routes/notificationRoute.js
+++ Server/routes/notificationRoute.js
@@ -1,23 +1,75 @@
 import express from "express";
+
 import {
   getNotifications,
   getNotificationById,
   deleteNotification,
   clearNotifications,
-    markAsRead,
-    markAllAsRead,
-    saveFcmToken,
+  markAsRead,
+  markAllAsRead,
+  saveFcmToken,
 } from "../controllers/notificationController.js";
+
 import { protect } from "../middleware/authMiddleware.js";
+
 
 const router = express.Router();
 
-router.get("/", getNotifications);
-router.get("/:id", getNotificationById);
-router.delete("/:id", deleteNotification);
-router.delete("/", clearNotifications);
-router.put("/:id/read", protect, markAsRead);
-router.put("/read-all", protect, markAllAsRead);
-router.post("/save-token", protect, saveFcmToken);
+
+// Get all notifications
+router.get(
+  "/",
+  protect,
+  getNotifications
+);
+
+
+// Get single notification
+router.get(
+  "/:id",
+  protect,
+  getNotificationById
+);
+
+
+// Delete notification
+router.delete(
+  "/:id",
+  protect,
+  deleteNotification
+);
+
+
+// Clear all notifications
+router.delete(
+  "/",
+  protect,
+  clearNotifications
+);
+
+
+// Mark one as read
+router.put(
+  "/:id/read",
+  protect,
+  markAsRead
+);
+
+
+// Mark all as read
+router.put(
+  "/read-all",
+  protect,
+  markAllAsRead
+);
+
+
+// Save Firebase token
+router.post(
+  "/save-token",
+  protect,
+  saveFcmToken
+);
+
 
 export default router;
