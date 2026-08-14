7dd98d0b58cc0d147c75f2f44ab58cd05f8dbb8f
kausikDas <kd96668@gmail.com>
2026-08-14 14:44:30 +0530

    Add Resend

--- Server/utils/sendEmail.js
+++ Server/utils/sendEmail.js
@@ -1,34 +1,21 @@
-import nodemailer from "nodemailer";
-import dns from "dns";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (to, subject, text) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-
-    dns: {
-      lookup: (hostname, options, callback) => {
-        dns.lookup(hostname, { family: 4 }, callback);
-      },
-    },
-  });
-
-  await transporter.verify();
-
-  console.log("SMTP Connected");
-
-  await transporter.sendMail({
-    from: `"Guitar In Soul" <${process.env.EMAIL}>`,
-    to,
+  const { data, error } = await resend.emails.send({
+    from: "Guitar In Soul <onboarding@resend.dev>",
+    to: [to],
     subject,
     text,
   });
+
+  if (error) {
+    console.error("Resend error:", error);
+    throw new Error(error.message);
+  }
+
+  console.log("Email sent:", data?.id);
 };
 
 export default sendEmail;
